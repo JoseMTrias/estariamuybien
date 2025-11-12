7dbed167c636c587685d398a97e752583a0cff3c
JoseM GT <jmgarciatrias@gmail.com>
2025-11-13 00:32:33 +0100

    aroma

--- src/App.jsx
+++ src/App.jsx
@@ -5,8 +5,8 @@ function App() {
   return (
     <>
       <div>
-        
-          <img src={myGif} className="myGif" alt="my Gif" />
+        <h1 className='maroma'>MAROMA, MÁNDAME EL AUDIO YA</h1>
+          {/* <img src={myGif} className="myGif" alt="my Gif" /> */}
         
       </div>
     </>
